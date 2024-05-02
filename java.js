5caf27a3b3ceefccd8ab87a3ff7fe3568c725c7a
Hyztax <168736601+Hyztax@users.noreply.github.com>
2024-05-02 13:57:46 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,42 @@
+function toggleColumnVisibility() {
+    var discordIframe = document.getElementById("discord-iframe");
+  
+    if (discordIframe.style.display === "none" || discordIframe.style.display === "") {
+      discordIframe.style.display = "block";
+    } else {
+      discordIframe.style.display = "none";
+    }
+  }
+  
+  
+  function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
+  
+  // Close the modal if the user clicks outside the modal content
+  window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+  function openModal() {
+    var isMobileDevice = /Mobi/i.test(window.navigator.userAgent);
+    if (isMobileDevice) {
+      alert("This device do not meet the requirements to run this action");
+    } else {
+      var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+    }
+  }
+  
+  
+    
+  
